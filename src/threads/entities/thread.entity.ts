fd9145fa99f09141cada89ca671ea864358662cf
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-10-10 09:09:01 -0300

    :sparkles: add: thread entity

--- src/threads/entities/thread.entity.ts
+++ src/threads/entities/thread.entity.ts
@@ -1,1 +1,26 @@
-export class Thread {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+@Entity()
+export class Thread {
+  // Attributes
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  id_user: number;
+
+  @Column()
+  title: string;
+
+  // Relationships
+  @ManyToOne(() => User, (user) => user.threads)
+  @JoinColumn({ name: 'id_user' })
+  user: User;
+}
