f75171b4af51216e169e5a2dc3a223ed14be45ae
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-10-24 19:57:23 -0300

    :card_file_box: update: using class transformer on user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
 import { Thread } from 'src/threads/entities/thread.entity';
 
 @Entity()
@@ -7,12 +8,15 @@ export class User {
   id: number;
 
   @Column()
+  @Expose({ name: 'user_name' })
   name: string;
 
   @Column({ unique: true })
+  @Expose({ name: 'user_email' })
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @OneToMany(() => Thread, (thread) => thread.user)
